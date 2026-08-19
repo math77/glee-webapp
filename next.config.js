49a4626d47a738a39d442e525f3fc536ccbe88b5
promatheus <matheusemanuel745@gmail.com>
2026-08-18 21:00:14 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {},
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false, encoding: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
