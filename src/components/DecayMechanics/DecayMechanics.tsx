f8aae0308906123cbe693b149ce7754ca310872b
promatheus <matheusemanuel745@gmail.com>
2026-09-09 00:10:18 -0300

    Update DecayMechanics.tsx

--- src/components/DecayMechanics/DecayMechanics.tsx
+++ src/components/DecayMechanics/DecayMechanics.tsx
@@ -21,7 +21,12 @@ const cards: [string, string, string][] = [
   [
     "04",
     "The continuation",
-    "GLEE unfolds through chapters (think new contracts/extensions), with each chapter introducing new layers to the experiment and building upon the systems established before it. So, GLEE is less of a static one-off NFT collection and more of a dynamic, evolving, programmable serie of pieces. Part canvas, part game, part puzzle, and part social system. Some of its rules are visible from the beginning. Others are meant to be discovered.",
+    "GLEE unfolds through chapters (think new contracts/extensions), with each chapter introducing new layers to the experiment and building upon the systems established before it.",
+  ],
+  [
+    "05",
+    "The continuation",
+    "So, GLEE is less of a static one-off NFT collection and more of a dynamic, evolving, programmable serie of pieces. Part canvas, part game, part puzzle, and part social system. Some of its rules are visible from the beginning. Others are meant to be discovered.",
   ],
 ];
 
